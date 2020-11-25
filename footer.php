0c256d3c7a399d5242b7f923787f6eda2a545e97
SteveNoyes <stevenmnoyes.af@gmail.com>
2020-11-25 09:25:38 -0800

    first commit

---
+++ footer.php
@@ -0,0 +1,10 @@
+  <div class="foot">
+    <div class="socials">
+      <img src="https://source.unsplash.com/random/40x40" alt="">
+      <img src="https://source.unsplash.com/random/40x40" alt="">
+      <img src="https://source.unsplash.com/random/40x40" alt="">
+    </div>
+    <p><?php echo 'Steven Noyes &copy; ' . date('Y')?></p>
+  </div>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title><?php echo $pageTitle?></title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <div class="wrapper">
+    <div class="header">
+      <div class="nav">
+        <ul>
+          <li class="logo"><a href="index.php">LOGO</a></li>
+          <li><a href="about.php">about</a></li>
+          <li><a href="service.php">service</a></li>
+          <li><a href="port.php">port</a></li>
+          <li><a href="contact.php">contact</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
